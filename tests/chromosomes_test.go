ebae1e4ab08dffef3519d32f086796b506041743
James Limmer <sidewaiise@gmail.com>
2021-07-05 20:31:11 +1000

    [tests] Adds test table and nanosecond timer

--- tests/chromosomes_test.go
+++ tests/chromosomes_test.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"testing"
+	"time"
 )
 
 func TestChromosomers(t *testing.T) {
 	t.Run("we can run a test", func(tt *testing.T) {
-
+		time.Sleep(time.Millisecond * 1500)
 	})
 }
